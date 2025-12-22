7d0966114a104df94aa2969055d07782829b88ff
Ben Du <longendu@yahoo.com>
2025-12-21 16:30:46 -0800

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,13 @@ config.initial_rows = 28
 config.font_size = 11
 config.color_scheme = "Tokyo Night"
 
+local HOME = os.getenv("HOME")
 local dimmer = { brightness = 0.1 }
 config.background = {
 	-- This is the deepest/back-most layer. It will be rendered first
 	{
 		source = {
-			--File = "/home/dclong/Downloads/wallpaper/outer_space.jpg",
-			File = "/home/dclong/Downloads/wallpaper/robot_castle.jpg",
+			File = HOME .. ".config/icon-data/wezterm/wallpaper/robot_castle.jpg",
 		},
 		-- The texture tiles vertically but not horizontally.
 		-- When we repeat it, mirror it so that it appears "more seamless".
